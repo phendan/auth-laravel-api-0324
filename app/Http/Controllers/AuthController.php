81b0abd3a6ea8f09ef7b4070604c23472b88120c
Philip Braunen <pbraunen@gmail.com>
2024-05-28 17:23:08 +0200

    Finish authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules\Password as PasswordRule;
 
@@ -26,5 +27,37 @@ class AuthController extends Controller
         ]);
 
         return response()->json(status: 201, data: [ 'user' => $user ]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string'
+        ]);
+
+        $loginSuccessful = Auth::attempt($request->only('email', 'password'));
+
+        if (!$loginSuccessful) {
+            $error = ['errors' => [ 'root' => ['We were unable to sign you in with those details.'] ]];
+            return response()->json(status: 422, data: $error);
+        }
+
+        $request->session()->regenerate();
+        return response()->json(status: 201, data: Auth::user());
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json(status: 204);
+    }
+
+    public function user(Request $request)
+    {
+        return $request->user();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
